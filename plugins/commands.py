765276404b91f34a348aa053b52100c5de91f379
DEEZUZZA-OFFLINE <112531339+DEEZUZZA-OFFLINE@users.noreply.github.com>
2022-09-20 09:33:38 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -239,7 +239,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='youtube.com/OpusTechz') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('💠 Gʀᴏᴜᴘ 💠', url='https://t.me/cinema_city_group') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
